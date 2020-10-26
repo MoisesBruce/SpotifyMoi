bc286ae0b97bde9b27f0679145c53656d6b554d3
felipefeleol <felima.oleveiira@gmail.com>
2020-10-26 11:41:12 -0400

    moises

---
+++ app/src/main/java/com/example/spotifymoi/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.spotifymoi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView ivMusic;
+    Button btnPlay;
+    MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ivMusic = (ImageView) findViewById(R.id.ivMusic);
+        Glide.with(this).load("https://img.youtube.com/vi/WIglwWHamm8/0.jpg").into(ivMusic);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.moisexs);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mediaPlayer.start();
+    }
+}
